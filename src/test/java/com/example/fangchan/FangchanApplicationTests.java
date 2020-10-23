968cf1d8b7e12d09a0f685614e84f355bdd31211
魏子皓 <575852846@qq.com>
2020-10-23 09:03:00 +0800

    first

---
+++ src/test/java/com/example/fangchan/FangchanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fangchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FangchanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
